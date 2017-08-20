cdea0bf5c440694f56a5ae449b8aa85116c86108
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-08-20 23:01:49 +0530

    Added comments

--- 1_pandas_introduction/1_pandas_introduction.py
+++ 1_pandas_introduction/1_pandas_introduction.py
@@ -6,13 +6,21 @@ from matplotlib import style
 
 style.use('ggplot')
 
+# Specify the start and end date for which the data will be fetched
 start = datetime.datetime(2010, 1, 1)
 end   = datetime.datetime(2017, 1, 1)
 
-df = web.DataReader("XOM", "yahoo", start, end)
+# Fetch data from yahoo finance for apple stock 
+df = web.DataReader("AAPL", "yahoo", start, end)
 
+# Get the first five entries 
 print(df.head())
 
+# Get the last five entries
+print(df.tail())
+
+# plot Ajusted close data 
 df['Adj Close'].plot()
 
+# display the plotted graph
 plt.show()
